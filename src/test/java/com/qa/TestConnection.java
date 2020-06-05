3cc1e5b26ee03b7ebf76c5fe84cc3a3ddc2cf6c7
SHANIMCD <shanilmcdonald@gmail.com>
2020-06-05 09:50:06 +0100

    commented out the test file

--- src/test/java/com/qa/TestConnection.java
+++ src/test/java/com/qa/TestConnection.java
@@ -10,29 +10,29 @@ import java.sql.Statement;
 
 public class TestConnection {
 	
-	static final String SCHEMA_LOCATION = "src\\test\\resources\\SchemaFile.sql";
-	static final String DATA_LOCATION = "src\\test\\resources\\dataFile.sql";
-
-	private static void sendToDB(Connection connection, String fileLocation) {
-        try (BufferedReader br = new BufferedReader(new FileReader(fileLocation));) {
-            String string;
-            while ((string = br.readLine()) != null) {
-                try (Statement statement = connection.createStatement();) {
-                    statement.executeUpdate(string);
-                }
-            }
-        } catch (SQLException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
-	@BeforeClass
-	public static void init() {
-		sendToDB(DriverManager.getConnection("jdbc:mysql://35.197.245.66/inventorydb", "root", "_takeaguess"), SCHEMA_LOCATION);
-	}
-	
-	@Before
-	public void setUp() {
-		JDBC = new TestConnection
-	}
+//	static final String SCHEMA_LOCATION = "src\\test\\resources\\SchemaFile.sql";
+//	static final String DATA_LOCATION = "src\\test\\resources\\dataFile.sql";
+//
+//	private static void sendToDB(Connection connection, String fileLocation) {
+//        try (BufferedReader br = new BufferedReader(new FileReader(fileLocation));) {
+//            String string;
+//            while ((string = br.readLine()) != null) {
+//                try (Statement statement = connection.createStatement();) {
+//                    statement.executeUpdate(string);
+//                }
+//            }
+//        } catch (SQLException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//	
+//	@BeforeClass
+//	public static void init() {
+//		sendToDB(DriverManager.getConnection("jdbc:mysql://35.197.245.66/inventorydb", "root", "_takeaguess"), SCHEMA_LOCATION);
+//	}
+//	
+//	@Before
+//	public void setUp() {
+//		JDBC = new TestConnection
+//	}
 }
